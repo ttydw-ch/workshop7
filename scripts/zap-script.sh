57e1c8b972f66f9c30188c95608d0817d375656b
ttydw-ch <1356774181@qq.com>
2026-08-21 13:56:22 +0800

    Update zap-script.sh to include report generation

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bit"
-
+docker run -v "$(pwd):/zap/wrk:rw" -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bit" -r zap_baseline_report.html
 echo $? > /dev/null
