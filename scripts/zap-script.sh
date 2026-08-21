f405c7c8abd1bcd28206691fd2dec64a198e5c07
ttydw-ch <1356774181@qq.com>
2026-08-21 14:09:35 +0800

    Update zap-script.sh

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 docker pull zaproxy/zap-stable
-docker run -v "$(pwd):/zap/wrk:rw" -i zaproxy/zap-stable zap-baseline.py -t "https://kenken64.github.io/bit" -r zap_baseline_report.html
+
+docker run -i --rm \
+  -v "$(pwd):/zap/wrk:rw" \
+  zaproxy/zap-stable \
+  zap-baseline.py \
+  -t "https://kenken64.github.io/bit" \
+  -r zap_baseline_report.html
+
 echo $? > /dev/null
