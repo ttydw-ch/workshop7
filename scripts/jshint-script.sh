a8d1b8b50401215209e45a417634eaced5d8a906
ttydw-ch <1356774181@qq.com>
2026-08-21 14:38:26 +0800

    Update jshint-script.sh

--- scripts/jshint-script.sh
+++ scripts/jshint-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-jshint --exclude="node_modules/" --reporter=unix . > JSHint-report
+jshint --exclude="node_modules/" --reporter=unix . > JSHint-report 2>&1
+status=$?
 
-echo $? > /dev/null
+echo "JSHint scan completed with exit code: $status" >> JSHint-report
+
+exit 0
